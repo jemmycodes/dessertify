6c1b7c88f8f466445b5fe708966b84550905ae23
Jemimah Otebele <flashyjotebele@gmail.com>
2024-01-17 01:03:05 +0100

    feat:doing something on the search functionality

--- app/_components/ui/Search.tsx
+++ app/_components/ui/Search.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 import { type ChangeEvent } from "react";
 import { FaSearch } from "react-icons/fa";
@@ -17,11 +18,8 @@ const Search = ({ smHidden, search, onSearch }: SearchProps) => {
   const handleSearch = async (e: ChangeEvent<HTMLInputElement>) => {
     setStatus("loading");
     onSearch(e.target.value);
-    console.log(search);
 
     const { data: menu, error } = await fetchFilteredMenu(e.target.value);
-
-    console.log(menu, error);
 
     !error && setSearchResults(menu);
 
@@ -29,7 +27,8 @@ const Search = ({ smHidden, search, onSearch }: SearchProps) => {
       setStatus("error");
       return;
     }
-    console.log(status, searchResults);
+
+    setStatus("idle");
   };
 
   return (
@@ -42,11 +41,36 @@ const Search = ({ smHidden, search, onSearch }: SearchProps) => {
       <input
         type="text"
         value={search}
-        onChange={(e) => handleSearch(e)}
+        onChange={(e) => void handleSearch(e)}
         placeholder="Search"
         className="border rounded-md w-full  text-black px-4 py-2 focus:outline-none  focus:border-orange/70 text-sm "
       />
       <FaSearch className="absolute top-[30%] right-4 text-gray-400 text-sm" />
+      {search?.length > 0 && (
+        <ul className="bg-white w-full max-w-md  shadow-lg p-4 text-sm absolute h-screen max-h-48  overflow-scroll scrollbar-hide flex flex-col gap-4 rounded-lg">
+          {status === "loading" && <p className="text-center">Loading...</p>}
+
+          {status === "error" && (
+            <p className="text-center">An error occurred!</p>
+          )}
+
+          {search.length > 0 && searchResults?.length === 0 && (
+            <p>No results found</p>
+          )}
+
+          {searchResults &&
+            searchResults.length > 0 &&
+            searchResults.map((results) => (
+              <Link
+                href={`/menu/${results.category}/dessert/${results._id}`}
+                key={results._id}>
+                <li className="hover:text-orange/70 cursor-pointer">
+                  {results.name}
+                </li>
+              </Link>
+            ))}
+        </ul>
+      )}
     </div>
   );
 };
